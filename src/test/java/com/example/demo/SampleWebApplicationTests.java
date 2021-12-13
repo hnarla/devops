52f7b9699117ba5a78a57af1df18902a1b010ade
Administrator <Administrator@gmail.com>
2021-12-13 22:42:03 +0530

    fjhfds

--- src/test/java/com/example/demo/SampleWebApplicationTests.java
+++ src/test/java/com/example/demo/SampleWebApplicationTests.java
@@ -1,13 +1,5 @@
-package com.example.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SampleWebApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
